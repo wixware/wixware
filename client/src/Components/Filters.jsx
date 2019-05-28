9c71c905bf745fdbb05a1ae8a8a509a03995c2e8
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 23:08:08 +0500

    .

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -4,39 +4,41 @@ import { Link } from 'react-router-dom';
 function Filters() {
   return (
     <div className='container-fluid hide-on-mobile'>
+      <div className='row'>
+        <div className='col-12 ml-5 mr-5'>
+          <h4 className='text-dark'>Filter by Collections / Categories</h4>
+        </div>
+      </div>
       <div className='row ml-auto mr-auto'>
-        <a className='btn btn-sm col-xl-2 col-lg-2 col-md-4 col-sm-4 col-xs-12 align-self-center'>
-          <span>Filter by Collections</span>
+        <a href='/' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>All</Link>
         </a>
-        <a href='/' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>All</span>
-        </a>
-        <Link to='/category/media-players' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>MediaPlayers</span>
+        <Link to='/category/media-players' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>MediaPlayers</Link>
         </Link>
-        <Link to='/category/antivirus' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Antivirus</span>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Antivirus</Link>
         </Link>
-        <Link to='/category/internet-browsers' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Browsers</span>
+        <Link to='/category/internet-browsers' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Browsers</Link>
         </Link>
-        <Link to='/category/video-editors' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>VideoEditors</span>
+        <Link to='/category/video-editors' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>VideoEditors</Link>
         </Link>
-        <Link to='/category/file-compressors' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Compressors</span>
+        <Link to='/category/file-compressors' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Compressors</Link>
         </Link>
-        <Link to='/category/downloaders' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Downloaders</span>
+        <Link to='/category/downloaders' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Downloaders</Link>
         </Link>
-        <Link to='/category/photo-editors' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>PhotoEditors</span>
+        <Link to='/category/photo-editors' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>PhotoEditors</Link>
         </Link>
-        <Link to='/category/for-developers' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Developers</span>
+        <Link to='/category/for-developers' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Developers</Link>
         </Link>
-        <Link to='/category/drivers' className='btn btn-secondary btn-sm col-xl-1 col-lg-2 col-md-4 col-sm-4 col-xs-12'>
-          <span>Drivers</span>
+        <Link to='/category/drivers' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 pr-2 pl-2'>
+          <Link>Drivers</Link>
         </Link>
       </div>
     </div>
