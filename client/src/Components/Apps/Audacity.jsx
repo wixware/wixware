b5abacf07570891682aa94a15b3fa9bf86668e8c
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 22:08:47 +0500

    .

--- client/src/Components/Apps/Audacity.jsx
+++ client/src/Components/Apps/Audacity.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import SearchBar from '../SearchBar';
 import ItemVLC from '../Items/VLC';
 import Item7Zip from '../Items/7Zip';
 import ItemVSCode from '../Items/VSCode';
@@ -38,7 +37,6 @@ function Audacity() {
         <meta name='description' content="Download Audacity and/or more software from wixware.com" />
         <meta name='keywords' content='audacity, download audacity, free audacity, latest audacity, sound editor' />
       </Helmet>
-      <SearchBar/>
       <div className='container-fluid mt-5 pl-5 pr-5'>
         <div className='row'>
           <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 box pl-5 pr-5 pt-5 pb-5'>
