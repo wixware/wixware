2600619d7a016dcc4995ff17e166015b4c9d1771
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 21:51:11 +0500

    .

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function SearchBar() {
   return (
     <div className='container text-center mt-5 mb-5'>
-      <h3 className='display-3 mt-5 mb-5'>Looking for something?</h3>
+      <h3 className='display-4 mt-5 mb-5'>Get the tools you need!</h3>
       <form action='/search' method='get' className='mt-4'>
         <div className='form-group'>
-          <input type="search" name="keyword" id='keyword' className='searchbar pt-4 pb-4 form-control' placeholder='Type something and press Enter to search' autoComplete='off' />
+          <input type="search" name="keyword" id='keyword' className='searchbar pt-4 pb-4 form-control' placeholder='Search Wixware' autoComplete='off' />
         </div>
       </form>
     </div>
