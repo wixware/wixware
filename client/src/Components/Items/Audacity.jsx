27af1f69ce7c97a46853734bd9f1b9d71b7ddebf
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 00:35:02 +0500

    .

--- client/src/Components/Items/Audacity.jsx
+++ client/src/Components/Items/Audacity.jsx
@@ -8,7 +8,7 @@ function ItemAudacity() {
         <div className='col-12'>
           <img src="https://cdn.appzaib.com/public/wixware/img/audacity.png" className='img-fluid pt-4 pl-4 pr-4' alt="Download Audacity" />
         </div>
-        <div className='col-12 text-muted small mb-1 mt-2'>
+        <div className='col-12 text-muted small mb-1 mt-2 text-center'>
           <strong className='bold'>Audacity</strong>
         </div>
         <div className='col-12 bg-primary text-white pt-1 pb-1'>
