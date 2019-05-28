53ba6a3324509cb66c9d6624a698903a6539d81d
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 02:07:12 +0500

    .

--- client/src/Components/Apps/Audacity.jsx
+++ client/src/Components/Apps/Audacity.jsx
@@ -39,6 +39,7 @@ function Audacity() {
         <meta name='description' content="Download Audacity and/or more software from wixware.com" />
         <meta name='keywords' content='audacity, download audacity, free audacity, latest audacity, sound editor' />
       </Helmet>
+      <Filters/>
       <div className='container-fluid mt-5 pl-5 pr-5'>
         <div className='row'>
           <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 box pl-5 pr-5 pt-5 pb-5'>
