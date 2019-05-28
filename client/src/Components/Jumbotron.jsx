7806d97cfe3bac7ed38db7be38e42dbd9969079c
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 00:08:17 +0500

    .

--- client/src/Components/Jumbotron.jsx
+++ client/src/Components/Jumbotron.jsx
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 
 function Jumbotron() {
   return (
-    <div className='jumbotron bg-primary text-white text-center no-border-radius mb-auto'>
+    <div className='jumbotron bg-dark text-white text-center no-border-radius mb-auto'>
       <a href='/'><img src="https://cdn.appzaib.com/public/wixware/img/logo-185x50-light.png" alt="Wixware Logo"/></a>
     </div>
   );
