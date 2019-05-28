0c58af16f6723b92bca261e0dab99be48dd0e2e0
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 22:17:23 +0500

    .

--- client/src/Pages/Category/PhotoEditors.jsx
+++ client/src/Pages/Category/PhotoEditors.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Helmet from 'react-helmet';
 
-import SearchBar from '../../Components/SearchBar';
 import ItemInkscape from '../../Components/Items/InkScape';
 
 function CategoryPhotoEditors() {
