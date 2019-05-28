c87bcc1b626e9c0d582ba0ef5e398af34319a835
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 14:16:31 +0500

    .

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+const express = require('express');
+const app = express();
+
+// To handle HTTP Get request
+app.get('/', () => (req, res) {
+  console.log('GET REQUEST:');
+  res.send('Get Request');
+})
+
+app.put('/', () => (req, res) {
+  console.log("HTTP Put Request");
+  res.send("HTTP PUT Request");
+});
+
+app.post('/', () => (req, res) {
+  console.log("HTTP POST Request");
+  res.send("HTTP POST Request");  
+});
+
+app.delete('/', () => (req, res) {
+  console.log("HTTP DELETE Request");
+  res.send("HTTP DELETE Request");
+});
+
+// Start server on Port 8080
+let server = app.listen(8080, function() {
+  
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Server listening at http://%s:%s', host, port);
+
+})
