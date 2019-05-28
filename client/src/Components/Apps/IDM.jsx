921af232a95d8000211331a2dcdf237b1e4b525a
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 02:06:21 +0500

    .

--- client/src/Components/Apps/IDM.jsx
+++ client/src/Components/Apps/IDM.jsx
@@ -36,6 +36,7 @@ function IDM() {
         <meta name='description' content="Download IDM (Internet Download Manager) and/or more software from wixware.com" />
         <meta name='keywords' content='idm, download idm, free idm, latest idm, downloader, download manager' />
       </Helmet>
+      <Filters/>
       <div className='container-fluid mt-5 pl-5 pr-5'>
         <div className='row'>
           <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 box pl-5 pr-5 pt-5 pb-5'>
