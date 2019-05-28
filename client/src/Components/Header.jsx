4efbfb7acdf2a8ade5a3cb66b1c5406a55e2158b
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 02:17:40 +0500

    .

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,26 +7,26 @@ function Header() {
 
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top mr-0">
-  <a class="navbar-brand" href="/"><img src="https://cdn.appzaib.com/public/wixware/img/logo-185x50.png" height='30px' alt="Wixware Logo"/></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
-    aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/about">
-          About
-        </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="/contact">
-          Contact
-        </a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <a class="navbar-brand" href="/"><img src="https://cdn.appzaib.com/public/wixware/img/logo-185x50.png" height='30px' alt="Wixware Logo"/></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
+        aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <Link class="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link class="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
