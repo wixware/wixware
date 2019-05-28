7055c791ad79a1a9a2b714b9dc299af508808e41
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 02:06:00 +0500

    .

--- client/src/Components/Apps/KMPlayer.jsx
+++ client/src/Components/Apps/KMPlayer.jsx
@@ -36,6 +36,7 @@ function KMPlayer() {
         <meta name='description' content="Download KM Player and/or more software from wixware.com" />
         <meta name='keywords' content='km player, kmplayer, download km player, free download km player, video player' />
       </Helmet>
+      <Filters/>
       <div className='container-fluid mt-5 pl-5 pr-5'>
         <div className='row'>
           <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 box pl-5 pr-5 pt-5 pb-5'>
