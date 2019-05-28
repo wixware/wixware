d1adf18f8e6f746ebb374484b0991a9a3476e32e
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 22:33:30 +0500

    .

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -3,17 +3,38 @@ import { Link } from 'react-router-dom';
 
 function Filters() {
   return (
-    <div className='container-fluid ml-5 mr-5'>
+    <div className='container-fluid ml-auto mr-auto'>
       <div className='row'>
-        <div className='col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12'>
-          <Link to='/category/media-players' className='btn btn-light'>Media Players</Link>
-        </div>
-        <div className='col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12'>
-          <Link to='/category/antivirus' className='btn btn-light'>Antivirus</Link>
-        </div>
-        <div className='col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12'>
-          <Link to='/category/internet-browsers' className='btn btn-light'>Browsers</Link>
-        </div>
+        <Link to='/category/media-players' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Media Players</span>
+        </Link>
+        <Link to='/category/antivirus' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Antivirus</span>
+        </Link>
+        <Link to='/category/internet-browsers' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Browsers</span>
+        </Link>
+        <Link to='/category/video-editors' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Video Editors</span>
+        </Link>
+        <Link to='/category/file-compressors' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>File Compressors</span>
+        </Link>
+        <Link to='/category/downloaders' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Downloaders</span>
+        </Link>
+        <Link to='/category/photo-editors' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Photo Editors</span>
+        </Link>
+        <Link to='/category/for-developers' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>For Developers</span>
+        </Link>
+        <Link to='/category/cd-dvd' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>CD &amp; DVD</span>
+        </Link>
+        <Link to='/category/drivers' className='btn btn-light col-xl-1 col-lg-1 col-md-2 col-sm-4 col-xs-12'>
+          <span>Drivers</span>
+        </Link>
       </div>
     </div>
   );
