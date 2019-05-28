9768ae0b94b7377b6fb99bb3f01d44108c3db4b6
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 22:11:11 +0500

    .

--- client/src/Components/Apps/VSCode.jsx
+++ client/src/Components/Apps/VSCode.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import SearchBar from '../SearchBar';
 import ItemVLC from '../Items/VLC';
 import ItemBlender from '../Items/Blender';
 import ItemIDM from '../Items/ItemIDM';
