e7252fe814f63e6783c3b46668cc87560bbc6f7a
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 23:30:45 +0500

    .

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -9,18 +9,49 @@ function Filters() {
           <h4 className='text-dark'>Filter by Collections / Categories</h4>
         </div>
       </div>
-      <div className='row text-center mt-4 ml-auto mr-auto filters'>
-        <div className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt'>
-          <a href='/'>
-            <span className=''>All</span>
-          </a>
-        </div>
-        <div className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt'>
-          <Link to='/category/media-players'>
-            <span>MediaPlayers</span>
-          </Link>
-        </div>
-        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt'>
+      <div className='row text-center mt-4 pl-4 pr-4 filters'>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+      </div>
+      <div className='row text-center mt-2 pr-4 pl-4 filters'>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
+            Antivirus
+        </Link>
+        <Link to='/category/antivirus' className='col-xl-1 col-lg-2 col-md-4 col-4 col-xs-12 mr-5 ml-5 pt-2 pb-2 box-alt mb-4'>
             Antivirus
         </Link>
       </div>
