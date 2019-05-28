690d77033fe109fef9534fbdcd234a51ebcbae84
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 14:03:34 +0500

    .

---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let app = express();
+
+// To handle HTTP Get request
+app.get('/', function (req, res) {
+  console.log('GET REQUEST:');
+  res.send('Get Request');
+})
+
+app.put('/', function (req, res) {
+  console.log("HTTP Put Request");
+  res.send("HTTP PUT Request");
+});
+
+app.post('/', function (req, res) {
+  console.log("HTTP POST Request");
+  res.send("HTTP POST Request");  
+});
+
+app.delete('/', function (req, res) {
+  console.log("HTTP DELETE Request");
+  res.send("HTTP DELETE Request");
+});
+
+// Start server on Port 8080
+let server = app.listen(8080, function() {
+  
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Server listening at http://%s:%s', host, port);
+
+})
