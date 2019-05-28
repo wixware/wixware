6252c39b0029c780efe6fae490b277f744dff813
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 00:35:32 +0500

    .

--- client/src/Components/Items/Chrome.jsx
+++ client/src/Components/Items/Chrome.jsx
@@ -8,7 +8,7 @@ function ItemChrome() {
         <div className='col-12'>
           <img src="https://cdn.appzaib.com/public/wixware/img/chrome.png" className='img-fluid pt-4 pl-4 pr-4' alt="Download Google Chrome" />
         </div>
-        <div className='col-12 text-muted small mb-1 mt-2'>
+        <div className='col-12 text-muted small mb-1 mt-2 text-center'>
           <strong className='bold'>Google Chrome</strong>
         </div>
         <div className='col-12 bg-primary text-white pt-1 pb-1'>
