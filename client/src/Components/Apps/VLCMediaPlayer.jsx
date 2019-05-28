e2a4ccf31538e55bf966b9ca0d7419b8f42012cc
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-28 22:11:02 +0500

    .

--- client/src/Components/Apps/VLCMediaPlayer.jsx
+++ client/src/Components/Apps/VLCMediaPlayer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import SearchBar from '../SearchBar';
 import ItemCamtasia from '../Items/Camtasia';
 import Item7Zip from '../Items/7Zip';
 import ItemCCleaner from '../Items/CCleaner';
