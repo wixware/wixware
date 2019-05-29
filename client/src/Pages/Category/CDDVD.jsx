536fced5ffe24dee5988a57e62d4d155ab641341
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-05-29 07:16:17 +0500

    .

--- client/src/Pages/Category/CDDVD.jsx
+++ client/src/Pages/Category/CDDVD.jsx
@@ -7,14 +7,15 @@ import Filters from '../../Components/Filters';
 
 function CategoryCDDVD() {
   return (
-    <div className='container-fluid ml-5 mr-5 mt-5 text-center'>
+    <div className='container-fluid ml-5 pr-5 pt-5 text-center'>
       <Helmet>
         <title>CD & DVD - Wixware</title>
         <meta name='description' content="We've a handful of best cd & dvd rom software categorized just for you. Select any and start downloading for free." />
         <meta name='keywords' content='cd burner, dvd burner, dvd software free download' />
         <meta name='author' content='Wixware Team' />
       </Helmet>
-      <div className='row'>
+      <Filters/>
+      <div className='row mt-5 pl-5 pr-5'>
         <div className='col-12 align-self-center'>
           <h4>Hi, we're working on a few things. Try again later.</h4>
         </div>
