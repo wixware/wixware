2adf18a717e84c6395314d12a35ea6a356d843ad
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-06-02 07:50:39 +0500

    .

--- index.js
+++ index.js
@@ -20,7 +20,32 @@ admin.initializeApp({
   credential: admin.credential.applicationDefault()
 });
 
+// Initializing Firestore
 const db = admin.firestore();
+
+let docRef = db.collection('users').doc('ranajahanzaib');
+
+// This is how you write data into Firestore
+// let setRJ = docRef.set({
+//   first_name: 'Rana',
+//   last_name: 'Jahanzaib',
+//   email: 'iam@ranajahanzaib.com'
+// });
+
+
+// This is how you GET data from Firestore
+let appRef = db.collection('apps').doc('zero');
+let getDoc = appRef.get()
+  .then(doc => {
+    if(!doc.exists) {
+      console.log('No such document found');
+    } else {
+      console.log('Document data:', doc.data());
+    }
+  })
+  .catch(err => {
+    console.log('Error getting document', err);
+  });
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
