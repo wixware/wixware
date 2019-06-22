d24b27a2f7af92510afc2922be402eff01b94417
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-06-22 13:28:21 +0500

    .

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -10,61 +10,61 @@ function Filters() {
         </div>
       </div>
       <div id="demo" className='collapse row text-center pl-5 pr-5 mt-4 filters'>
-        <a href='/' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <a href='/' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-infinity'></i>
           <br/>
           <br/>
             All
         </a>
-        <Link to='/category/media-players' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/media-players' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-play'></i>
           <br/>
           <br/>
             Media Players
         </Link>
-        <Link to='/category/antivirus' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/antivirus' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-shield-alt'></i>
           <br/>
           <br/>
             Antivirus
         </Link>
-        <Link to='/category/internet-browsers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/internet-browsers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-globe'></i>
           <br/>
           <br/>
             Browsers
         </Link>
-        <Link to='/category/video-editors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/video-editors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-video'></i>
           <br/>
           <br/>
             Video Editors
         </Link>
-        <Link to='/category/file-compressors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/file-compressors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-compress'></i>
           <br/>
           <br/>
             Compressors
         </Link>
-        <Link to='/category/downloaders' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/downloaders' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-download'></i>
           <br/>
           <br/>
             Downloaders
         </Link>
-        <Link to='/category/photo-editors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/photo-editors' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-images'></i>
           <br/>
           <br/>
             Photo Editors
         </Link>
-        <Link to='/category/for-developers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/for-developers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-code'></i>
           <br/>
           <br/>
             Developers
         </Link>
-        <Link to='/category/drivers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-5 pb-5 box-alt-2 mb-4'>
+        <Link to='/category/drivers' className='col-xl-2 col-lg-2 col-md-2 col-xs-12 pt-4 pb-4 box-alt-2 mb-4'>
           <i className='display-4 fa fa-wrench'></i>
           <br/>
           <br/>
--- client/src/Components/Items/VLC.jsx
+++ client/src/Components/Items/VLC.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { stringify } from 'querystring';
 
 class ItemVLC extends Component {
 
